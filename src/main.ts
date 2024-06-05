d4235b1bf8281d495768dbe796d27481236ec140
gaurang-agileinfoways <gaurang.patel@agileinfoways.com>
2024-06-05 17:59:20 +0530

    feat: database configuration

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
-    transport: Transport.TCP,
+    transport: Transport.REDIS,
     options: {
-      port: 3100,
+      host: 'localhost',
+      port: 6379,
     },
   });
+
+  //Enable API Params ValidationPipe for all endpoint.
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen();
 }
 bootstrap();
